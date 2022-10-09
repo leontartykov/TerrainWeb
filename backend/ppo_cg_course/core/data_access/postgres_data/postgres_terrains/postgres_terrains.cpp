c66682f7b6d3e6a2fecc80ff1faf4e48e5c2344c
Lev <lev_trtykov@mail.ru>
2022-10-09 23:20:08 +0300

    add: добавлен функционал на методы get и put terrain api

--- backend/ppo_cg_course/core/data_access/postgres_data/postgres_terrains/postgres_terrains.cpp
+++ backend/ppo_cg_course/core/data_access/postgres_data/postgres_terrains/postgres_terrains.cpp
@@ -1,5 +1,31 @@
 #include "postgres_terrains.h"
 
-void TerrainProjectsPostgres::set_terrain_params(terrain_t &terrain_params){
-    __terrain_params = terrain_params;
+void TerrainProjectsPostgres::set_psql_connection(std::shared_ptr<pqxx::connection> &conn_psql){
+    __conn_psql = conn_psql;
+}
+
+void TerrainProjectsPostgres::set_terrain_params(std::string &project_name, terrain_t &terrain_params){
+    pqxx::work worker(*__conn_psql);
+    std::string query = "UPDATE terrain_project.terrains.terrain SET ('" + \
+                         std::to_string(terrain_params.width) + "', '" + \
+                         std::to_string(terrain_params.height) + "', '" + \
+                         std::to_string(terrain_params.scale) + "', '" + \
+                         std::to_string(terrain_params.meta_config.octaves) + "', '" + \
+                         std::to_string(terrain_params.meta_config.gain) + "', '" + \
+                         std::to_string(terrain_params.meta_config.lacunarity) + "', '" + \
+                         std::to_string(terrain_params.meta_config.seed) + "', '" + \
+                         std::to_string(terrain_params.meta_config.frequency) + "', '" + \
+                         std::to_string(terrain_params.rotate_angles.angle_x) + "', '" + \
+                         std::to_string(terrain_params.rotate_angles.angle_y) + "', '" + \
+                         std::to_string(terrain_params.rotate_angles.angle_z) + \
+                         "WHERE terrain_project.terrains.terrain = " + project_name + "';'";
+    worker.exec(query);
+    worker.commit();
+}
+
+void TerrainProjectsPostgres::get_project_terrain()
+{
+    pqxx::work worker(*__conn_psql);
+    std::string query = "SELECT * FROM terrain_project.terrains.terrain";
+    worker.exec(query);
 }
--- backend/ppo_cg_course/core/data_access/postgres_data/postgres_terrains/postgres_terrains.h
+++ backend/ppo_cg_course/core/data_access/postgres_data/postgres_terrains/postgres_terrains.h
@@ -1,6 +1,8 @@
 #ifndef _POSTGRES_TERRAINS_H_
 #define _POSTGRES_TERRAINS_H_
 
+#include <pqxx/pqxx>
+
 #include "../../../object/terrain/terrain_struct.h"
 #include "../../../heightmap/perlin_noise/perlin_noise_struct.h"
 
@@ -16,12 +18,19 @@ struct terrain
 class TerrainProjectsPostgres
 {
     private:
+        std::string __project_name;
         terrain_t __terrain_params;
+
+        std::shared_ptr<pqxx::connection> __conn_psql;
 
     public:
         TerrainProjectsPostgres() = default;
         ~TerrainProjectsPostgres() = default;
-        void set_terrain_params(terrain_t &terrain_params);
+
+        void set_psql_connection(std::shared_ptr<pqxx::connection> &conn_psql);
+
+        void set_terrain_params(std::string &project_name, terrain_t &terrain_params);
+        void get_project_terrain();
 };
 
 #endif
