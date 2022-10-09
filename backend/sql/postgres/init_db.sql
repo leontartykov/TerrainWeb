4ec0b3f32cfb3fd1dfb569a5ae618d9fdb3527bd
Lev <lev_trtykov@mail.ru>
2022-10-09 23:21:26 +0300

    update: переназваны БД, схемы и таблицы; добавлены новые таблицы terrain

--- backend/sql/postgres/init_db.sql
+++ backend/sql/postgres/init_db.sql
@@ -1,12 +1,49 @@
-CREATE DATABASE IF NOT EXISTS Users;
+CREATE DATABASE IF NOT EXISTS terrain_project;
+CREATE SCHEMA terrain_project.users;
 
-CREATE TABLE IF NOT EXISTS Users.passwords
+CREATE TABLE IF NOT EXISTS terrain_project.users.passwords
 (
-	login varchar primary key;
-	password varchar;
-	blocked boolean;
-	deleted boolean;
+	id int primary key,
+	login varchar primary key,
+	password varchar,
+	blocked boolean,
+	deleted boolean
 );
 
-INSERT INTO Users.passwords values
-('admin', 'admin', false, false);
+INSERT INTO terrain_project.users.passwords values
+(0, 'admin', 'admin', false, false);
+
+CREATE TABLE IF NOT EXISTS terrain_project.terrains.terrain
+(
+	id int primary key,
+	name varchar primary key,
+	last_edited date,
+
+	project_id int,
+	foreign key(project_id) references terrain_project.terrains.projects(id)
+);
+
+CREATE TABLE IF NOT EXISTS terrain_project.terrains.terrains_users
+(
+	id_user int,
+	foreign key(id_user) references terrain_project.users.passwords(id),
+
+	id_terrain int,
+	foreign key(id_terrain) references terrain_project.terrains.terrain(id)
+);
+
+CREATE TABLE IF NOT EXISTS terrain_project.terrains.projects
+(
+	id int primary key;
+	width int,
+	height int,
+	scale double precision,
+	octaves int,
+	gain double precision,
+	lacunarity double precision,
+	seed int,
+	frequency double precision,
+	angle_x int,
+	angle_y int,
+	angle_z int
+);
