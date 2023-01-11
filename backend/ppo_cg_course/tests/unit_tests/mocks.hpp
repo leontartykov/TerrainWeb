ee5c95674f3e403642873b3da31031a0bd87cc8a
Lev <lev_trtykov@mail.ru>
2023-01-11 14:45:43 +0300

    update: добавлены новые методы для моков

--- backend/ppo_cg_course/tests/unit_tests/mocks.hpp
+++ backend/ppo_cg_course/tests/unit_tests/mocks.hpp
@@ -28,6 +28,7 @@ class MockDb: public DbModel
 
         MOCK_METHOD(int, add_new_terrain_project, (const std::string &userName, const std::string &terProjName), (override));
         MOCK_METHOD(int, get_terrain_params, (const std::string &userName, const std::string &projName, servTerrain_t &terParams), (override));
+        MOCK_METHOD(int, get_terrain_params, (const std::string &projName, servTerrain_t &terParams), (override));
         MOCK_METHOD(int, get_terrain_projects, (const std::string &userName, int &page, std::vector<servTerrainProject_t> &servTerProjects), (override));
         MOCK_METHOD(int, save_terrain_params, (const std::string &userName, const std::string &projName, const servTerrain_t &servTer), (override));
         MOCK_METHOD(int, get_terrain_project, (const std::string &projName, servTerrainProject_t &servTerProjects, const std::string &userName), (override));
@@ -36,6 +37,7 @@ class MockDb: public DbModel
         MOCK_METHOD(int, set_terrain_project_rating, (const std::string &projName, const std::string &userName), (override));
         MOCK_METHOD(int, add_project_for_rating, (const std::string &userName, const std::string &projName), (override));
         MOCK_METHOD(int, get_all_rating_projects, (const std::string &page, std::vector<servTerrainProject_t> &terProjects), (override));
+        MOCK_METHOD(void, set_db, (const std::string &dbName), (override));
 };
 struct MyDatabase
 {
